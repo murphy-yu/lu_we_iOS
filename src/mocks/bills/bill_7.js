873e8b49e9994a73c9b7854b083af76c7292fd66
Murphy Yu <yuwj86@126.cpm>
2018-08-26 11:29:30 +0800

    [FEA] Adding mock data

---
+++ src/mocks/bills/bill_7.js
@@ -0,0 +1,126 @@
+import wepy from 'wepy';
+
+
+let m_bill = {
+  calenderArr: ['2018', '07'],   // 当前年月
+  moneyStat: {
+    income: '777.00',   // 收入
+    pay: '77.00'       // 支出
+  },
+  detailList: [
+    {
+      info: {
+        date: '07月25日',
+        day: '星期日',
+        totalIncome: 0,
+        totalPay: 0
+      },
+      list: [
+        {
+          id: 1,
+          image: '/images/reading.png',
+          title: '餐饮',
+          remark: '',
+          mount: -12
+        },
+        {
+          id: 2,
+          image: '/images/reading.png',
+          title: '兼职',
+          remark: '',
+          mount: 20
+        },
+        {
+          id: 1,
+          image: '/images/reading.png',
+          title: '餐饮',
+          remark: '',
+          mount: -12
+        },
+        {
+          id: 2,
+          image: '/images/reading.png',
+          title: '兼职',
+          remark: '',
+          mount: 20
+        }
+      ]
+    },
+    {
+      info: {
+        date: '07月25日',
+        day: '星期日',
+        totalIncome: 0,
+        totalPay: 0
+      },
+      list: [
+        {
+          id: 1,
+          image: '/images/reading.png',
+          title: '餐饮',
+          remark: '',
+          mount: -12
+        },
+        {
+          id: 2,
+          image: '/images/reading.png',
+          title: '兼职',
+          remark: '',
+          mount: 20
+        }
+      ]
+    },
+    {
+      info: {
+        date: '07月25日',
+        day: '星期日',
+        totalIncome: 0,
+        totalPay: 0
+      },
+      list: [
+        {
+          id: 1,
+          image: '/images/reading.png',
+          title: '餐饮',
+          remark: '',
+          mount: -12
+        },
+        {
+          id: 2,
+          image: '/images/reading.png',
+          title: '兼职',
+          remark: '',
+          mount: 20
+        }
+      ]
+    },
+    {
+      info: {
+        date: '07月25日',
+        day: '星期日',
+        totalIncome: 0,
+        totalPay: 0
+      },
+      list: [
+        {
+          id: 1,
+          image: '/images/reading.png',
+          title: '餐饮',
+          remark: '',
+          mount: -12
+        },
+        {
+          id: 2,
+          image: '/images/reading.png',
+          title: '兼职',
+          remark: '',
+          mount: 20
+        }
+      ]
+    }
+  ],
+  // 滚动区域信息
+  scrollHeight: 0
+}
+
+export default m_bill
---
+++ src/mocks/bills/bill_8.js
@@ -0,0 +1,126 @@
+import wepy from 'wepy';
+
+
+let m_bill = {
+  calenderArr: ['2018', '02'],   // 当前年月
+  moneyStat: {
+    income: '888.00',   // 收入
+    pay: '88.00'       // 支出
+  },
+  detailList: [
+    {
+      info: {
+        date: '08月25日',
+        day: '星期日',
+        totalIncome: 0,
+        totalPay: 0
+      },
+      list: [
+        {
+          id: 1,
+          image: '/images/reading.png',
+          title: '餐饮',
+          remark: '',
+          mount: -12
+        },
+        {
+          id: 2,
+          image: '/images/reading.png',
+          title: '兼职',
+          remark: '',
+          mount: 20
+        },
+        {
+          id: 1,
+          image: '/images/reading.png',
+          title: '餐饮',
+          remark: '',
+          mount: -12
+        },
+        {
+          id: 2,
+          image: '/images/reading.png',
+          title: '兼职',
+          remark: '',
+          mount: 20
+        }
+      ]
+    },
+    {
+      info: {
+        date: '08月25日',
+        day: '星期日',
+        totalIncome: 0,
+        totalPay: 0
+      },
+      list: [
+        {
+          id: 1,
+          image: '/images/reading.png',
+          title: '餐饮',
+          remark: '',
+          mount: -12
+        },
+        {
+          id: 2,
+          image: '/images/reading.png',
+          title: '兼职',
+          remark: '',
+          mount: 20
+        }
+      ]
+    },
+    {
+      info: {
+        date: '08月25日',
+        day: '星期日',
+        totalIncome: 0,
+        totalPay: 0
+      },
+      list: [
+        {
+          id: 1,
+          image: '/images/reading.png',
+          title: '餐饮',
+          remark: '',
+          mount: -12
+        },
+        {
+          id: 2,
+          image: '/images/reading.png',
+          title: '兼职',
+          remark: '',
+          mount: 20
+        }
+      ]
+    },
+    {
+      info: {
+        date: '08月25日',
+        day: '星期日',
+        totalIncome: 0,
+        totalPay: 0
+      },
+      list: [
+        {
+          id: 1,
+          image: '/images/reading.png',
+          title: '餐饮',
+          remark: '',
+          mount: -12
+        },
+        {
+          id: 2,
+          image: '/images/reading.png',
+          title: '兼职',
+          remark: '',
+          mount: 20
+        }
+      ]
+    }
+  ],
+  // 滚动区域信息
+  scrollHeight: 0
+}
+
+export default m_bill
